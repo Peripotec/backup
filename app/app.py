a6959b8a1ecc377c2dad6922a3a49f8201bb865e
David M. Maidana <david.marcelo.maidana@gmail.com>
2025-01-09 12:00:37 -0300

    Etapa 2: Interfaz web base con navegación y estilos básicos

--- app/app.py
+++ app/app.py
@@ -1,11 +1,15 @@
 # app/app.py
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "¡Sistema de Backups en marcha!"
+    return render_template("index.html")
+
+@app.route("/admin")
+def admin_dashboard():
+    return "<h2>Panel de Administración</h2>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
